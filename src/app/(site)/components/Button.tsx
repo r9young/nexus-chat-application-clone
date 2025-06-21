8a0b94d7f2d360d8aa1110624e30c498ec58d3b3
r9young <121323016+r9young@users.noreply.github.com>
2025-06-21 09:57:00 +0900

    feat: enhance Button component with additional props for better customization

--- src/app/(site)/components/Button.tsx
+++ src/app/(site)/components/Button.tsx
@@ -7,28 +7,56 @@ interface ButtonProps {
     children?: React.ReactNode;
     onClick?:() => void;
     type?:'button' | 'submit' | 'reset' | undefined;
+
+    // short-circuit evaluation
     disabled?: boolean;
+    danger?:boolean;
+    secondary?: boolean;
 
 
 }
 
 const Button: React.FC<ButtonProps> = ({
+    type = 'button',
     fullWidth = false,
     children,
     onClick, // did not pass any handle function, e.g onClick={()=> alter('Clicked!')}
-    disabled = false
+
+    // short-circuit evaluation
+    disabled = false,
+    danger = false,
+    secondary = false,
 
 }) => {
     return (
         <button
-            type='button'
+            type={type}
             onClick={onClick}
             className={clsx(
+                `
+                    flex
+                    justify-center
+                    rounded-md
+                    px-3
+                    py-2
+                    text-sm
+                    font-semibold
+                    focus-visible:outline
+                    focus-visible:outline-2
+                    focus-visible:outline-offset-2
+                `,
+
                 fullWidth && 'w-full',
-                disabled && 'opacity-50 cursor-default'
+                disabled && 'opacity-50 cursor-default',
+                danger &&
+                    'bg-rose-500 hover;bg-rose-600 focus-visible:outline-rose-600',
+                // if both secondary and danger are not ture
+                !secondary && !danger && 'bg-cyan-500 hover:bg-cyan-600 focus-visible:outline-cyan-600'
             )}
         >
             {children}
         </button>
     )
 }
+
+export default Button;
