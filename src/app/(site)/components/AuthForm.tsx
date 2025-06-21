7095857326f603e0a5b9f5b6e762ebe4a98d79d4
r9young <121323016+r9young@users.noreply.github.com>
2025-06-21 14:33:19 +0900

    feat: implement AuthForm component with session handling and toggle functionality

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -4,15 +4,40 @@
 // import Button from '@/app/components/Button'
 import Button from './Button'
 // import Input from '@/app/components/input/Input'
+import input from './input/Input'
 
 
 import { useState, useCallback, useEffect } from 'react';
 import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
-import AuthSocialButton from './AuthSocialButton';
+// import AuthSocialButton from './AuthSocialButton';
 
 
+// new hooks
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
+import { signIn, useSession } from 'next-auth/react';
+
+type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+
+    const session = useSession();
+    const router = useRouter();
+    const [variant, setVariant] = useState<Variant>('LOGIN')// it give an initial value to variant, which is LOGIN and also give a type for safe which is Variant.
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session.status === 'authenticated') {
+            router.push('/users')
+        }
+    },[session?.status, router]);
+
+
+    const toggleVariant = useCallback (()=> {
+        setVariant((prev) => (prev === 'LOGIN' ? 'REGISTER' : 'LOGIN'))
+    }, [variant]);
+
+
 
     return (
         <div></div>
