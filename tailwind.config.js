740f1eae9dd8dc196276162524176149a7da2dc3
r9young <121323016+r9young@users.noreply.github.com>
2025-06-24 12:42:51 +0900

    fix: comment out unused file extension patterns in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    // "./app/**/*.{js,ts,jsx,tsx}",
+    // "./pages/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+    // "./src/app/*.{js,ts,jsx,tsx}",
+    // './app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/*.{js,ts,jsx,tsx}",
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx}"
+
+
   ],
   theme: {
     extend: {},
