dbbb98f05c286c8df028c21924dfda83c9dc0592
r9young <121323016+r9young@users.noreply.github.com>
2025-06-25 12:16:20 +0900

    fix: correct rendering of DesktopSidebarItem components in DesktopSidebar

--- src/app/(site)/components/sidebar/DesktopSidebar.tsx
+++ src/app/(site)/components/sidebar/DesktopSidebar.tsx
@@ -23,7 +23,10 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
             <nav className="mt-4 flex flex-col justify-between">
                 <ul role="list" className="flex flex-col items-center space-y-1">
-                    {routes}
+                    {/*  iterates over the array returned by useRoutes and for each route it creates a DesktopSidebarItem component */}
+                    {routes}.map((route) => {
+                        <DesktopSidebarItem />
+                    })
 
                 </ul>
             </nav>
