6681087a61cdd4b3624325fc8533ea33069845cb
r9young <121323016+r9young@users.noreply.github.com>
2025-07-01 16:08:28 +0900

    feat: implement getSession function and initialize Prisma client

--- src/app/actions/getUsers.ts
+++ src/app/actions/getUsers.ts
@@ -1,6 +1,32 @@
+// app/action/getUsers.ts
+
+import prisma from '@/app/libs/prismadb';
+import getSession from './getSession';
 
 const getUsers = async () => {
+  // try {
+  //   const session = await getSession();
 
-}
+  //   if (!session?.user?.email) return [];
 
-export default getUsers
+  //   // Get all users except the current user
+  //   const users = await prisma.user.findMany({
+  //     orderBy: {
+  //       createdAt: 'desc',
+  //     },
+  //     where: {
+  //       NOT: {
+  //         email: session.user.email as string,
+  //       },
+  //     },
+  //   });
+
+  //   if (!users) return [];
+
+  //   return users;
+  // } catch (error: any) {
+  //   return [];
+  // }
+};
+
+export default getUsers;
