7829d9035eb646c688a9f0272f307c2b74d9d0cc
r9young <121323016+r9young@users.noreply.github.com>
2025-07-02 12:26:05 +0900

    refactor: clean up getUsers and prismadb files by removing commented code and improving comments

--- src/app/actions/getUsers.ts
+++ src/app/actions/getUsers.ts
@@ -1,32 +1,3 @@
-// app/action/getUsers.ts
+import client from '../libs/prismadb'
 
-import prisma from '@/app/libs/prismadb';
-import getSession from './getSession';
-
-const getUsers = async () => {
-  // try {
-  //   const session = await getSession();
-
-  //   if (!session?.user?.email) return [];
-
-  //   // Get all users except the current user
-  //   const users = await prisma.user.findMany({
-  //     orderBy: {
-  //       createdAt: 'desc',
-  //     },
-  //     where: {
-  //       NOT: {
-  //         email: session.user.email as string,
-  //       },
-  //     },
-  //   });
-
-  //   if (!users) return [];
-
-  //   return users;
-  // } catch (error: any) {
-  //   return [];
-  // }
-};
-
-export default getUsers;
+//  in this file you are importing the Prisma client instance from @/app/libs/prismadb and using it to interact with (manipulate/query) the database.
--- src/app/libs/prismadb.ts
+++ src/app/libs/prismadb.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+// the purpose of this file is to create an instance
+
+import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -6,5 +8,10 @@ declare global {
 
 const client = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis['prisma'] = client;
+// avoid hot reload in development environment
 
-export default client;
+
+export default client; 
+
+
+// then next step will go to getUssers.tsx to use the instance and muniplate the database
