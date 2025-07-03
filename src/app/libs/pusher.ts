0b3d8f5166fafb8361ed15db769244cdc23c2b4a
r9young <121323016+r9young@users.noreply.github.com>
2025-07-03 11:58:34 +0900

    feat: implement Pusher server and client setup for real-time communication

---
+++ src/app/libs/pusher.ts
@@ -0,0 +1,33 @@
+import PusherServer from 'pusher'
+import PusherClient from 'pusher-js'
+
+
+// PUSHER_APP_ID
+// NEXT_PUBLIC_PUSHER_APP_KEY
+// PUSHER_SECRET
+// NEXT_PUBLIC_PUSHER_APP_KEY
+// cluster
+// useTLS
+
+export const PusherServer = new PusherServer({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: 'ap2',
+    useTLS: true,
+})
+
+// exmaple of how to apply pusherServer
+// e.g await pusherServer.trigger(conversationId, 'messages:new', newMessage);
+
+
+export const pusherClient = new PusherClient (
+    process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    {
+    channelAuthorization: {
+      endpoint: '/api/pusher/auth',
+      transport: 'ajax',
+    },
+    cluster: 'ap2',
+    }
+)
