85ea6ea1a4fc47206a00551d53f903b884bcb410
r9young <121323016+r9young@users.noreply.github.com>
2025-07-07 19:13:54 +0900

    chore: update comments to reference issue #116 for clarity

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -1,5 +1,6 @@
 // In this file, we are going to fetch the data from the prisma.
 // making two special data type for other component to be used. 
+// see issue #116
 
 import { Conversation, Message, User } from '@prisma/client'
 
